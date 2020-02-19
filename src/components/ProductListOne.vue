35c91267753379d5c8904c08752a108c59cf2a29
Tri-Vi <36706183+Tri-Vi@users.noreply.github.com>
2020-02-19 11:06:14 -0600

    BOD - 02/19/20 -04
    @tri-vi
    
    - Implement actions

--- src/components/ProductListOne.vue
+++ src/components/ProductListOne.vue
@@ -7,7 +7,7 @@
         <span class="price"> ${{product.price}} </span>
       </li>
     </ul>
-    <button @click="reducePrice">Reduce Price</button>
+    <button @click="reducePrice(4)">Reduce Price</button>
   </div>
 </template>
 
@@ -30,11 +30,12 @@ export default {
     }
   },
   methods: {
-    reducePrice(){
+    reducePrice(amount){
       // this.$store.state.products.forEach(product => {
       //   product.price -= 1;
       // });
-      this.$store.commit('reducePrice');
+      //this.$store.commit('reducePrice');
+      this.$store.dispatch('reducePrice', amount);
     }
   }
 }
